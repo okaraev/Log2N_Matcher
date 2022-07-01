b404f32bfad0a1486b63f7a63db21b1736de90f6
Ogtay Garayev <okaraev@outlook.com>
2022-07-01 22:26:57 +0400

    Fixed issue with retrier

--- messageQ.go
+++ messageQ.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"errors"
+	"log"
 	"os"
 	"time"
 
@@ -116,4 +117,81 @@ func (b *Breaker) Do(AMQPServer string, QName string, i interface{}) error {
 func (b *Breaker) Close() {
 	b.Status = "Closed"
 	b.FailCount = 0
+}
+
+type Retrier struct {
+	Status      string        // Retrier Status
+	HoldTime    time.Duration // Time duration after which Operation will retry. If Operation fails hold time multiplies until 360 seconds
+	MaxHoldTime time.Duration
+	Operation   func(q string, qq string) (<-chan amqp.Delivery, error)
+}
+
+func (r *Retrier) New(myFunc func(q string, qq string) (<-chan amqp.Delivery, error)) {
+	r.HoldTime = 10 * time.Second
+	r.MaxHoldTime = 360 * time.Second
+	r.Status = "Closed"
+	r.Operation = myFunc
+}
+
+func (r *Retrier) Open() {
+	r.Status = "Open"
+}
+
+func (r *Retrier) Close() {
+	r.HoldTime = 10 * time.Second
+	r.Status = "Closed"
+}
+
+func (r *Retrier) Multiply() {
+	if r.HoldTime <= r.MaxHoldTime {
+		r.HoldTime = r.HoldTime * 2
+	}
+}
+
+func (r *Retrier) isClosed() bool {
+	return r.Status == "Closed"
+}
+
+func (r *Retrier) Do(RMQServer string, QName string) (<-chan amqp.Delivery, error) {
+	result, err := r.Operation(RMQServer, QName)
+	mydelivery := make(chan amqp.Delivery)
+	if err != nil {
+		r.Open()
+		log.Println(err)
+	}
+	if r.isClosed() {
+		// if status is success starting routine to receive messages
+		go func() {
+			for message := range result {
+				mydelivery <- message
+			}
+			// Ending routine when error happened and setting status to fail
+			r.Open()
+		}()
+	}
+	// Starting forever loop in routine to check if status is fail
+	go func() {
+		for {
+			time.Sleep(1 * time.Second)
+			if !r.isClosed() {
+				// if status is fail waiting for defined hold time and trying again
+				time.Sleep(r.HoldTime)
+				retryResult, err := r.Operation(RMQServer, QName)
+				if err != nil {
+					r.Multiply()
+				} else {
+					// if retry is success setting status to success and starting routine to receive messages
+					r.Close()
+					go func() {
+						for message := range retryResult {
+							mydelivery <- message
+						}
+						// Ending routine when error happened and setting status to fail
+						r.Open()
+					}()
+				}
+			}
+		}
+	}()
+	return (<-chan amqp.Delivery)(mydelivery), nil
 }
